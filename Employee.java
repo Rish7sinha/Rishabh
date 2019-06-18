42df348211d65c29bc7692e2c8feebb9bbe31c1e
Rish7sinha <36431844+Rish7sinha@users.noreply.github.com>
2019-06-19 00:09:25 +0530

    Employee Information

---
+++ Employee.java
@@ -0,0 +1,14 @@
+public class Employee {
+
+        String first_name;
+        String last_name;
+        String department;
+        String gender;
+        int contact_number;
+        String aid;
+        String emailAddress;
+        int yearOfBirth;
+        int salary;
+        String Education
+
+}
