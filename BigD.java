198042bde5113360c6aa2bbea6d7826274c08922
Rish7sinha <36431844+Rish7sinha@users.noreply.github.com>
2019-06-19 09:40:33 +0530

    Addition using Big Decimal

---
+++ BigD.java
@@ -0,0 +1,24 @@
+import java.math.BigDecimal;
+
+public class BigD {
+
+    public static void main(String[] args) {
+
+
+        BigDecimal bg1,bg2,bg3;
+
+
+        bg1 = new BigDecimal("10.732");
+        bg2 = new BigDecimal("3.12");
+
+
+        System.out.println("Object Value is " + bg1);
+        System.out.println("Augend value is " + bg2);
+
+
+        bg3 = bg1.add(bg2);
+
+
+        System.out.println("Result is " + bg3);
+    }
+}
